866b7e8e0f353a4182e203337a2125144e2c1961
Josh Jacoby <joshuadjacoby@gmail.com>
2017-09-20 01:02:02 -0700

    Removed php file, doesn't work with S3. Added support for formspree.io

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "https://formspree.io/joshuadjacoby@gmail.com",
         type: "POST",
         data: {
           name: name,
@@ -28,6 +28,7 @@ $(function() {
           email: email,
           message: message
         },
+		dataType: "json",
         cache: false,
         success: function() {
           // Success message
